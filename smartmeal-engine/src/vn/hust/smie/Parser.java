e12c7558ff40d601c6f0e8bb50d3e61f4482f1f2
lequana2 <lequana2@gmail.com>
2013-11-11 22:14:02 +0700

    # change to inputstream
    Signed-off-by: lequana2 <lequana2@gmail.com>

--- smartmeal-engine/src/vn/hust/smie/Parser.java
+++ smartmeal-engine/src/vn/hust/smie/Parser.java
@@ -1,93 +1,80 @@
 package vn.hust.smie;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
 
 public class Parser {
 
-    /**
-     * Parse data file and create an IngredientCollection
-     * 
-     * @param fileName
-     * @return
-     */
-    public static IngredientCollection parseIngredient(String fileName) {
-	IngredientCollection ic = new IngredientCollection();
-	BufferedReader br = null;
-	String line;
-	char ch;
-	
-	try {
-	    br = new BufferedReader(new FileReader(fileName));
-	    while ((line = br.readLine()) != null) {
-		line = line.trim();
-		if (line.length() <= 0) continue;
-		ch = line.charAt(0);
-		if (ch == '\n' || ch == '#') continue;
-		
-		String[] pattern = line.split(",");
-		ic.addIngredient(new Ingredient(pattern[0], pattern[1], pattern[2], pattern[3],
-			pattern[4], pattern[5], pattern[6], pattern[7]));
-	    }
-	} catch (IOException e) {
-	    e.printStackTrace();
-	} finally {
-	    if (br != null) try {
-		br.close();
-	    } catch (IOException e) {
-		e.printStackTrace();
-	    }
-	}
-	
-	return ic;
-    }
-    
-    public static DishCollection parseDish(IngredientCollection ic, String fileName) {
-	DishCollection dc = new DishCollection();
-	BufferedReader br = null;
-	String line;
-	char ch;
-	int i;
-	
-	try {
-	    br = new BufferedReader(new FileReader(fileName));
-	    while ((line = br.readLine()) != null) {
-		line = line.trim();
-		if (line.length() <= 0) continue;
-		ch = line.charAt(0);
-		if (ch == '\n' || ch == '#') continue;
-		
-		String[] pattern = line.split(",");
-		Dish dish = new Dish(pattern[0], pattern[1], pattern[2], pattern[3], pattern[4]);
-		
-		// Add components to dish
-		i = 5;
-		while (true) {
-		    try {
-			Ingredient ing = ic.getIngredient(Integer.parseInt(pattern[i]));
-			double amount = Double.parseDouble(pattern[i+ 1]);
-			dish.addComponent(new DishComponent(ing, amount));
-			i += 2;
-		    } catch (Exception e) {
-			break;
-		    }
+	/**
+	 * Parse data file and create an IngredientCollection
+	 * 
+	 * @param inputStream
+	 * @return
+	 */
+	public static IngredientCollection parseIngredient(InputStream inputStream) {
+		IngredientCollection ic = new IngredientCollection();
+		try{
+			List<String> lines = IOUtils.readLines(inputStream);
+			char ch;
+			for (String line : lines){
+				line = line.trim();
+				if (line.length() <= 0) continue;
+				ch = line.charAt(0);
+				if (ch == '\n' || ch == '#') continue;
+
+				String[] pattern = line.split(",");
+				ic.addIngredient(new Ingredient(pattern[0], pattern[1], pattern[2], pattern[3],
+						pattern[4], pattern[5], pattern[6], pattern[7]));
+			}
 		}
-		
-		// Add dish to collection
-		dc.addDish(dish);
-	    }
-	} catch (IOException e) {
-	    e.printStackTrace();
-	} finally {
-	    if (br != null) try {
-		br.close();
-	    } catch (IOException e) {
-		e.printStackTrace();
-	    }
+		catch (IOException e){
+			e.printStackTrace();
+		}
+
+		return ic;
 	}
-	
-	return dc;
-    }
-    
+
+	public static DishCollection parseDish(IngredientCollection ic, InputStream inputStream) {
+		DishCollection dc = new DishCollection();
+		try{
+			List<String> lines = IOUtils.readLines(inputStream);
+			char ch;
+			int i;
+			for (String line : lines){
+				line = line.trim();
+				if (line.length() <= 0) continue;
+				ch = line.charAt(0);
+				if (ch == '\n' || ch == '#') continue;
+
+				String[] pattern = line.split(",");
+				Dish dish = new Dish(pattern[0], pattern[1], pattern[2], pattern[3], pattern[4]);
+
+				// Add components to dish
+				i = 5;
+				while(true){
+					try{
+						Ingredient ing = ic.getIngredient(Integer.parseInt(pattern[i]));
+						double amount = Double.parseDouble(pattern[i + 1]);
+						dish.addComponent(new DishComponent(ing, amount));
+						i += 2;
+					}
+					catch (Exception e){
+						break;
+					}
+				}
+
+				// Add dish to collection
+				dc.addDish(dish);
+			}
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+
+		return dc;
+	}
+
 }
